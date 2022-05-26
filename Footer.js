8b5ff3d6cd1bd6319c3bd554f7549e9b2aaae9f3
kiks717 <96001243+kiks717@users.noreply.github.com>
2022-05-26 18:15:20 +0200

    Update Footer.js

--- Footer.js
+++ Footer.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitterSquare, faGithubSquare, faInstagramSquare, faLinkedinIn } from "@fortawesome/free-brands-svg-icons" 
-// import { LinkHTMLAttributes } from 'react';
 
 export default function Footer () {
     return (
